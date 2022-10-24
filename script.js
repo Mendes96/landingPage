39065cd5701ff6c7f6eb897d25ab7feddecd61e5
Mendes96 <116464958+Mendes96@users.noreply.github.com>
2022-10-24 19:53:42 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,96 +1,48 @@
-var page1 = document.querySelector(".home");
-var page2 = document.querySelector(".about");
-var page3 = document.querySelector(".projetos");
-var page4 = document.querySelector(".contatos");
-var btHome = document.querySelector(".bt-home");
-p = document.createElement("p");
-page1.appendChild(p)
-p.innerHTML = "INICIO";
+var page1 = document.querySelector(".page1");
+var page2 = document.querySelector(".page2");
+var page3 = document.querySelector(".page3");
 
-
-function pg1(){
-  page1.style.transition =  "0.5s";
-  page1.style.width = "70%";
+function home(){
+  page1.style.width = "100%";
+  page1.style.transition = "0.5s";
+  page1.style.opacity = "1";
   
-  page2.style.transition =  "0.5s";
-  page2.style.width = "0";
-  
-  page3.style.transition =  "0.5s";
-  page3.style.width = "0px";
-  
-  page4.style.transition =  "0.5s";
-  page4.style.width = "0px";
-}
-
-function pg2(){
-  page2.style.transition =  "0.5s";
-  page2.style.width = "70%";
-  
-  page1.style.transition =  "0.5s";
-  page1.style.width = "0px";
-  
-  page3.style.transition =  "0.5s";
-  page3.style.width = "0px";
-  
-  page4.style.transition =  "0.5s";
-  page4.style.width = "0px";
-}
-
-function pg3(){
-  page3.style.transition =  "0.5s";
-  page3.style.width = "70%";
-  
-  page1.style.transition =  "0.5s";
-  page1.style.width = "0px";
-  
-  page2.style.transition =  "0.5s";
   page2.style.width = "0px";
+  page2.style.transition = "0.5s";
+  page2.style.opacity = "0";
   
-  page4.style.transition =  "0.5s";
-  page4.style.width = "0px";
-}
-
-function pg4(){
-  page4.style.transition =  "0.5s";
-  page4.style.width = "70%";
-  
-  page1.style.transition =  "0.5s";
-  page1.style.width = "0px";
-  
-  page2.style.transition =  "0.5s";
-  page2.style.width = "0px";
-  
-  page3.style.transition =  "0.5s";
   page3.style.width = "0px";
+  page3.style.transition = "0.5s";
+  page3.style.opacity = "0";
+  
 }
-
-btHome.addEventListener("click", function home(){
-  pg1()
-  //se a Pagina conter P, ele não cria um novo
-  if(!page1.contains(p)){
-    p = document.createElement("p");
-    page1.appendChild(p)
-    p.innerHTML = "INICIO";
-  }else{}
-})
 
 function about(){
-  pg2()
-  if(page1.contains(p)){
-    page1.removeChild(p);
-  }
+  page2.style.width = "100%";
+  page2.style.transition = "0.5s";
+  page2.style.opacity = "1";
+  
+  page1.style.width = "0"
+  page1.style.opacity = "0";
+  page1.style.transition = "0.5s";
+  
+  page3.style.width = "0"
+  page3.style.opacity = "0";
+  page3.style.transition = "0.5s";
+  
 }
 
-function projetos(){
-  pg3()
-  if(page1.contains(p)){
-    page1.removeChild(p);
-  }
-}
-
-function contatos(){
-  pg4()
-  if(page1.contains(p)){
-    page1.removeChild(p);
-  }
+function shop(){
+  page3.style.width = "100%";
+  page3.style.transition = "0.5s";
+  page3.style.opacity = "1";
+  
+  page1.style.width = "0"
+  page1.style.opacity = "0";
+  page1.style.transition = "0.5s";
+  
+  page2.style.width = "0"
+  page2.style.opacity = "0";
+  page2.style.transition = "0.5s";
+  
 }
